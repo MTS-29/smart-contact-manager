d20fe8ca6ffa43afac87efdffce6291e304cf55e
MTS-29 <mtaquisiddiqui@prologis.com>
2024-05-29 18:51:13 +0530

    Dark theme implementation

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,22 +3,52 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-    
-    @RequestMapping("/home")
-    public String home(Model model){
-        System.out.println("Home page handeller");
 
-        // Sending data to view
-        model.addAttribute("name", "Substring array");
-        model.addAttribute("youtube", "Dummy channel");
-        model.addAttribute("youtubelink", "https://www.youtube.com/watch?v=3p__WB2Kuls&list=PL0zysOflRCemNS641tpw66bcaFylyIGsA&index=6");
-        model.addAttribute("github", "Dummy github");
-        return "Home"; 
-        
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("Home page handler");
+        // sending data to view
+        model.addAttribute("name", "Substring Technologies");
+        model.addAttribute("youtubeChannel", "Learn Code With Durgesh");
+        model.addAttribute("githubRepo", "https://github.com/learncodewithdurgesh/");
+        return "home";
+    }
+
+    // about route
+
+    @RequestMapping("/about")
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", true);
+        System.out.println("About page loading");
+        return "about";
+    }
+
+    // services
+
+    @RequestMapping("/services")
+    public String servicesPage() {
+        System.out.println("services page loading");
+        return "services";
+    }
+
+    // contact page
+
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
     }
 }
