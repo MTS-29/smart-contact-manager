b972a6475c24976a4d713ce6c9a86a6c8c4906a4
MTS-29 <mtaquisiddiqui@prologis.com>
2024-07-05 12:13:51 +0530

    [Github Login] Github Login functionality completed

--- src/main/java/com/scm/config/OAuthAuthenticationSuccessHandler.java
+++ src/main/java/com/scm/config/OAuthAuthenticationSuccessHandler.java
@@ -1,13 +1,14 @@
 package com.scm.config;
 
 import java.io.IOException;
-import java.util.UUID;
 import java.util.List;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -34,33 +35,70 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
                 logger.info("OAuthAuthenticationSuccessHandler");
+                
+                // Identify the provider
+                var OAuthAuthenticationToken = (OAuth2AuthenticationToken) authentication;              
+                String authorizedClientRegistrationId =  OAuthAuthenticationToken.getAuthorizedClientRegistrationId();
 
-                DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
+                logger.info("Client: "+authorizedClientRegistrationId);
 
-                String email = user.getAttribute("email").toString();
-                String name = user.getAttribute("name").toString();
-                String picture = user.getAttribute("picture").toString();
+
+                var oathUser = (DefaultOAuth2User)authentication.getPrincipal();
+
+                oathUser.getAttributes().forEach((key, value) ->{
+                    logger.info(key + " : " + value);
+                });
 
                 User userSave = new User();
-                userSave.setName(name);
-                userSave.setEmail(email);
-                userSave.setProfilePic(picture);
-                userSave.setPassword("password");
                 userSave.setUserId(UUID.randomUUID().toString());
-                userSave.setProvider(Providers.GOOGLE);
-                userSave.setEnabled(true);
                 userSave.setEmailVerified(true);
-                userSave.setProviderUserId(user.getName());
+                userSave.setEnabled(true);
                 userSave.setRoleList(List.of(AppConstants.ROLE_USER));
-                userSave.setAbout("This account is created using google");
+                userSave.setPassword("dummy ");
 
-                User checkUser = userRepo.findByEmail(email).orElse(null);
+                if(authorizedClientRegistrationId.equalsIgnoreCase("google")){
+                    
+                    userSave.setName(oathUser.getAttribute("name").toString());
+                    userSave.setProfilePic(oathUser.getAttribute("picture").toString());
+                    userSave.setEmail(oathUser.getAttribute("email").toString());
+                    userSave.setProviderUserId(oathUser.getName());
+                    userSave.setProvider(Providers.GOOGLE);
+                    userSave.setAbout("Created by Google login");
+                }
+
+                else if(authorizedClientRegistrationId.equalsIgnoreCase("github")){
+                    String email = oathUser.getAttribute("email") != null ? oathUser.getAttribute("email").toString() : oathUser.getAttribute("login").toString() + "@gmail.com";
+                    String picture = oathUser.getAttribute("avatar_url").toString();
+                    String name = oathUser.getAttribute("login").toString();
+                    String providerUserId = oathUser.getName();
+
+                    userSave.setEmail(email);
+                    userSave.setProfilePic(picture);
+                    userSave.setName(name);
+                    userSave.setProvider(Providers.GITHUB);
+                    userSave.setProviderUserId(providerUserId);
+                    userSave.setAbout("Created by Github login");
+                }
+
+                // else if(authorizedClientRegistrationId.equalsIgnoreCase("linkedin")){
+                
+                // }
+
+                // else if(authorizedClientRegistrationId.equalsIgnoreCase("facebook")){
+
+                // }
+
+                else{
+                    logger.info("Unknown Client ");
+                }
+
+                 
+                User checkUser = userRepo.findByEmail(userSave.getEmail()).orElse(null);
 
                 if(checkUser == null){
                     userRepo.save(userSave);
-                    logger.info("User saved : "+ email);
+                    logger.info("User saved : "+ userSave.getEmail());
                 }
-
                 new DefaultRedirectStrategy().sendRedirect(request, response, "/user/profile ");
             }
 
