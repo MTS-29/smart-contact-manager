d24afc47ea6e0edc24c52f78e21a97d00289a5a2
MTS-29 <mtaquisiddiqui@prologis.com>
2024-07-09 10:46:41 +0530

    [Admin Page] Setup of the logged in user

--- src/main/java/com/scm/controllers/UserController.java
+++ src/main/java/com/scm/controllers/UserController.java
@@ -1,15 +1,12 @@
 package com.scm.controllers;
 
-import java.security.Principal;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.security.core.Authentication;
-
-import com.scm.helpers.Helper;
-
+ 
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -19,19 +16,14 @@ public class UserController {
     // user dashbaord page
     @RequestMapping(value = "/dashboard")
     public String userDashboard() {
-        System.out.println("User dashboard");
+       logger.info("User dashboard");
         return "user/dashboard";
     }
 
     // user profile page
 
     @RequestMapping(value = "/profile")
-    public String userProfile(Authentication authentication) {
-        
-        String userName = Helper.getEmailOfLoggedInUser(authentication);
-
-        logger.info("Logged inn: "+userName);
-        System.out.println("User profile");
+    public String userProfile(Model model, Authentication authentication) {
         return "user/profile";
     }
 
